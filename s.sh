b3700fb84ca0cdd8173fbc3e0d07a0a1aeb92dc5
sakz <fayecy@qq.com>
2017-08-30 01:35:40 +0800

    修改脚本

--- s.sh
+++ s.sh
@@ -2,16 +2,18 @@
 cd /etc/shadowsocks
 a=$RANDOM
 b=`expr $a \* $191`
-sed -i '/"138.*,$/d' config.json
-sed -i '2a \"138":"'${b}'",' config.json
+sed -i '/"2017.*,$/d' config.json
+sed -i '2a \"2017":"'${b}'",' config.json
 /etc/init.d/shadowsocks restart
 cd ~
 cd ss
 git pull
 git checkout gh-pages
-sed -i '59c \<h4>密码:'$b'</h4>' index.html
+sed -i '41c \<h4>密码:'$b'</h4>' index.html   #41行替换为
 time=`date "+%Y.%m.%d"`
+date=`date "+%m.%d"`
 sed -i '31c \<h6>账号更新时间：'$time'</h6>' index.html
+sed -i '43c  \<h4>状态:<font color="green">正常('$date'更新)</font></h4>' index.html #43行替换为
 git add index.html
 git commit -m "$time update"
 git push
